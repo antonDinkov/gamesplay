4515c40fd168321ef1da26bb800fe906deb4feb1
antonDinkov <166150125+antonDinkov@users.noreply.github.com>
2025-03-14 00:44:10 +0200

    catalog create details edit raw jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import Home from "./home/Home"
 import Navigation from "./Navigation"
 import Login from "./login/Login"
 import Register from "./register/Register"
+import Create from "./create/Create"
+import Catalog from "./catalog/Catalog"
+import Details from "./details/Details"
 
 
 function App() {
@@ -15,6 +18,9 @@ function App() {
                     <Route path="/" element={<Home />}></Route>
                     <Route path="/login" element={<Login/>}></Route>
                     <Route path="/register" element={<Register/>}></Route>
+                    <Route path="/create" element={<Create/>}></Route>
+                    <Route path="/catalog" element={<Catalog/>}></Route>
+                    <Route path="/catalog/details" element={<Details/>}></Route>
                 </Routes>
             </main>
 
